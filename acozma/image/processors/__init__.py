aee326f9301a7bd7c39bf9298ed2988fdaa54a93
Andrei Cozma <me@andreicozma.com>
2023-10-06 17:08:22 -0400

    Added contour colorgrid and refactored canny version of the postprocessing to match

--- acozma/image/processors/__init__.py
+++ acozma/image/processors/__init__.py
@@ -2,6 +2,7 @@ from .blur import *
 from .canny import *
 from .canny_colorgrid import *
 from .colorgrid import *
-from .contours import *
+from .contour import *
+from .contour_colorgrid import *
 from .entropy import *
 from .hog import *
--- acozma/image/processors/contour_colorgrid.py
+++ acozma/image/processors/contour_colorgrid.py
@@ -2,10 +2,11 @@ from PIL import Image, ImageChops, ImageEnhance, ImageOps
 
 from acozma.image.processors.canny import canny, rand_canny
 from acozma.image.processors.colorgrid import colorgrid, rand_colorgrid
+from acozma.image.processors.contour import contour, rand_contour
 from acozma.image.processors.utils import processor
 
 
-def _canny_colorgrid_post(image_bg: Image.Image, image_fg: Image.Image):
+def _contour_colorgrid_post(image_bg: Image.Image, image_fg: Image.Image):
     image_bg_inv = ImageOps.invert(image_bg.convert("L"))
     image_bg_inv = ImageEnhance.Contrast(image_bg_inv).enhance(factor=20)
 
@@ -17,18 +18,18 @@ def _canny_colorgrid_post(image_bg: Image.Image, image_fg: Image.Image):
 
 
 @processor
-def canny_colorgrid(image: Image.Image, grid_size=5):
+def contour_colorgrid(image: Image.Image, grid_size=5):
     image_bg = colorgrid(image, grid_size=grid_size)
-    image_fg = canny(image)
+    image_fg = contour(image)
 
-    return _canny_colorgrid_post(image_bg, image_fg)
+    return _contour_colorgrid_post(image_bg, image_fg)
 
 
-def rand_canny_colorgrid(image: Image.Image):
+def rand_contour_colorgrid(image: Image.Image):
     image_bg, colorgrid_params = rand_colorgrid(image)
-    image_fg, canny_params = rand_canny(image)
+    image_fg, canny_params = rand_contour(image)
 
     return (
-        _canny_colorgrid_post(image_bg, image_fg),
+        _contour_colorgrid_post(image_bg, image_fg),
         {**canny_params, **colorgrid_params},
     )
