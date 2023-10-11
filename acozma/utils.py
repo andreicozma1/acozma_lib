5df176cd98ad3c43625505f0d61990dad55875c2
Andrei Cozma <me@andreicozma.com>
2023-10-10 20:28:53 -0400

    Updates

--- acozma/utils.py
+++ acozma/utils.py
@@ -3,6 +3,8 @@ import re
 import string
 from collections import defaultdict
 
+import numpy as np
+
 from .decorators import listify
 from .string import remove_longest_repeating_substring
 
@@ -18,6 +20,18 @@ def in_notebook():
     except AttributeError:
         return False
     return True
+
+
+def min_max_scale(image):
+    min_val, max_val = np.min(image), np.max(image)
+    return (image - min_val) / (max_val - min_val)
+
+
+def nextpow2(N):
+    n = 1
+    while n < N:
+        n *= 2
+    return n
 
 
 @listify
