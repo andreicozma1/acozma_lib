7116f3f3a8f8aec1f3cbd83b2d47545adca0e25b
Andrei Cozma <me@andreicozma.com>
2023-10-26 16:07:44 -0400

    Updates

--- acozma/controlnet_processors/entropy.py
+++ acozma/controlnet_processors/entropy.py
@@ -8,27 +8,25 @@ from .utils import processor
 
 
 @processor
-def entropy(img, radius: int = 5, **kwargs):
+def entropy(img: Image.Image, radius: int = 5, **kwargs):
     assert radius >= 3, "radius must be non-negative"
 
-    img = np.array(img)
+    img_np = np.array(img)
     footprint = morphology.disk(radius)
 
     # compute entropy for each channel separately
     entropy_cs = []
-    for channel in range(img.shape[-1]):
-        ec = rank.entropy(img[..., channel], footprint=footprint)
+    for channel in range(img_np.shape[-1]):
+        ec = rank.entropy(img_np[..., channel], footprint=footprint)
         # ec = (ec - ec.min()) / (ec.max() - ec.min())
         entropy_cs.append(ec)
 
     # merge channels back into a single image
-    img = np.stack(entropy_cs, axis=-1)
-    img = (img - img.min()) / (img.max() - img.min())
+    img_np = np.stack(entropy_cs, axis=-1)
+    img_np = (img_np - img_np.min()) / (img_np.max() - img_np.min())
 
-    img = img_as_ubyte(img)
-    img = Image.fromarray(img)
-    # img = ImageOps.autocontrast(img)
-    return img
+    img_np = img_as_ubyte(img_np)
+    return Image.fromarray(img_np)
 
 
 def rand_entropy(image: Image.Image, **kwargs):
