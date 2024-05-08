14d0368318dd6b136df2c4f66072f10dcb8862a2
Andrei Cozma <me@andreicozma.com>
2024-05-08 17:00:54 -0400

    Updates

--- acozma/print/__init__.py
+++ acozma/print/__init__.py
@@ -1,2 +1,19 @@
 from .headers import *
 from .headers_md import *
+
+from pprint import pprint as _pprint
+from pprint import pformat as _pformat
+
+pprint_default_kwargs = dict(
+    depth=2, width=30, sort_dicts=False, compact=True, underscore_numbers=True
+)
+
+pprint = lambda *args, **kwargs: _pprint(
+    *args,
+    **(pprint_default_kwargs | kwargs),
+)
+
+pformat = lambda *args, **kwargs: _pformat(
+    *args,
+    **(pprint_default_kwargs | kwargs),
+)
